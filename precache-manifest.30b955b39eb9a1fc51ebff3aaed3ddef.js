255861e14c3592ac566f486edad554bb8c799964
Pramod Gupta <P10484337@capita.co.uk>
2021-02-19 20:33:00 +0530

    Updates

---
+++ precache-manifest.30b955b39eb9a1fc51ebff3aaed3ddef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b071cadfa942ceb4f3",
+    "url": "/facerecognitionbrain/static/css/main.fc557763.chunk.css"
+  },
+  {
+    "revision": "97b071cadfa942ceb4f3",
+    "url": "/facerecognitionbrain/static/js/main.127b1a13.chunk.js"
+  },
+  {
+    "revision": "f6347d204469216fb787",
+    "url": "/facerecognitionbrain/static/js/runtime~main.b33bd057.js"
+  },
+  {
+    "revision": "364bb31ca04f0860e33c",
+    "url": "/facerecognitionbrain/static/css/2.c66211f2.chunk.css"
+  },
+  {
+    "revision": "364bb31ca04f0860e33c",
+    "url": "/facerecognitionbrain/static/js/2.a3abc199.chunk.js"
+  },
+  {
+    "revision": "86e7405fc4746dce3146",
+    "url": "/facerecognitionbrain/static/js/3.c6802288.chunk.js"
+  },
+  {
+    "revision": "84256408e0a7f000ede1b7a4e7c9d98c",
+    "url": "/facerecognitionbrain/static/media/brain.84256408.png"
+  },
+  {
+    "revision": "0c6469c20bb95481d165d9529eca7786",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
